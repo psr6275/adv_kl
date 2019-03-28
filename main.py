aa923b9d95283f586f999b4fca20e76ed6d2717e
saerom <psr6275>
2019-03-28 13:53:39 +0900

    first draft

--- main.py
+++ main.py
@@ -168,7 +168,7 @@ def load_data(dataset = args.dataset,transform=args.transform , train_batch=args
 
     return trainloader, testloader
 
-def load_model(dataset = args.dataset,checkpoint = args.checkpoint, load_clf=args.load_clf):
+def load_model(dataset = args.dataset,checkpoint = args.checkpoint, load_clf=args.load_clf,type = 'recon'):
     if not os.path.isdir(checkpoint):
         mkdir_p(checkpoint)
 
@@ -193,6 +193,7 @@ def load_model(dataset = args.dataset,checkpoint = args.checkpoint, load_clf=arg
         )
 
     model_clf = nn.DataParallel(model_clf).cuda()
+    model_dae = nn.DataParallel(model_dae).cuda()
 
     # Model Load or Train!
     if load_clf:
@@ -201,8 +202,9 @@ def load_model(dataset = args.dataset,checkpoint = args.checkpoint, load_clf=arg
         args.checkpoint = os.path.dirname(load_clf)
         checkpoint = torch.load(load_clf)
         model_clf.load_state_dict(checkpoint['state_dict'])
-
-    return model_clf, model_dae
+    model_comb = models.combine_model(model_dae, model_clf,type)
+    model_comb = nn.DataParallel(model_comb)
+    return model_clf, model_dae, model_comb.cuda()
 
 
 
@@ -211,43 +213,19 @@ def train_clf(model,trainloader,testloader,criterion = nn.CrossEntropyLoss(), lr
     optimizer = optim.Adam(model.parameters(),lr=lr)
     best_acc = 0
     logger = Logger(os.path.join(checkpoint,'log.txt'),title = dataset)
-    logger.set_names(['Train Loss','Valid Loss', 'Train Acc', 'Valid Acc'])
+    logger.set_names(['Train Loss', 'Train Top1', 'Train Top3', 'Valid Loss', 'Valid Top1', 'Valid Top3'])
 
     for epoch in range(epochs):
-        # Switch train mode
-        model.train()
-        losses = AverageMeter()
-        top1 = AverageMeter()
-        top3 = AverageMeter()
 
-        print('\nEpoch: [%d |%d] ' % (epoch+1,epochs))
-        for batch_idx, (inputs,targets) in enumerate(trainloader):
-            inputs, targets = inputs.cuda(), targets.cuda()
-            inputs, targets = Variable(inputs),Variable(targets)
-
-            outputs = model(inputs)
-            loss = criterion(outputs, targets)
-
-            # print intermediate top1 and top3 error
-            prec1, prec3 = accuracy(outputs.data, targets.data,topk=(1,3))
-            if (batch_idx)% 10 == 1:
-                print("Training [Acc] top1: %.2f , top3: %.2f "%(100*prec1, 100*prec3))
-            losses.update(loss.data[0],inputs.size(0))
-            top1.update(prec1[0],inputs.size(0))
-            top3.update(prec3[0],inputs.size(0))
-
-
-            # Compute gradient and SGD step
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+        # Train classifier through one epoch
+        losses, top1, top3 = train_clf_step(model,trainloader, epoch, epochs, criterion, optimizer)
 
         # Calculate Test error!
         test_loss, test_acc, test_acc3 = test_clf(model, testloader, criterion)
         print('Training/Test [loss] %.4f / %.4f, [top1] %.2f / %.2f, [top3] %.2f / %.2f' %
               (losses.avg,test_loss, 100*top1.avg, 100*test_acc, 100*top3.avg, 100*test_acc3))
         #print('Test [loss] %.4f, [top1]')
-        logger.append([losses.avg,top1.avg,top3.avg])
+        logger.append([losses.avg,top1.avg,top3.avg, test_loss, test_acc,test_acc3])
         # save_model
         is_best = top1.avg > best_acc
         best_acc = max(test_acc, best_acc)
@@ -266,11 +244,12 @@ def train_clf(model,trainloader,testloader,criterion = nn.CrossEntropyLoss(), lr
     print('Best acc:')
     print(best_acc)
 
-def train_clf_step(model,trainloader,epoch,epochs,criterion,):
-    model.train()
+def train_clf_step(model,trainloader,epoch,epochs,criterion,optimizer):
+
     losses = AverageMeter()
     top1 = AverageMeter()
     top3 = AverageMeter()
+    model.train()
 
     print('\nEpoch: [%d |%d] ' % (epoch + 1, epochs))
     for batch_idx, (inputs, targets) in enumerate(trainloader):
@@ -293,11 +272,20 @@ def train_clf_step(model,trainloader,epoch,epochs,criterion,):
         loss.backward()
         optimizer.step()
 
-def train_dae(model_dae, model_clf, data_loader,dae_loss = args.dae_loss, lr = args.lr,
+    return losses, top1, top3
+
+def train_dae(model_dae, model_clf, model_comb, trainloader,testloader,dae_loss = args.dae_loss, lr = args.lr,
               epochs = 50, tempr = 10, std = 0.1, checkpoint='checkpoint'):
     assert dae_loss in ['KL, L2, L1','KL_reverse'], 'Error dae_loss should be in [KL, L2, L1, KL_reverse]'
+
     # Use classifier only for evaluation
     model_clf.eval()
+
+    best_acc = 0
+    logger = Logger(os.path.join(checkpoint, 'log.txt'), title=dataset)
+    logger.set_names(['Train Loss', 'Train Top1', 'Train Top3', 'Valid Loss','Valid Top1', 'Valid Top3'])
+
+    ## Training dae part only!
     optimizer = optim.Adam(model_dae.parameters(),lr=lr)
     if dae_loss == 'L1':
         criterion = nn.L1Loss()
@@ -310,34 +298,78 @@ def train_dae(model_dae, model_clf, data_loader,dae_loss = args.dae_loss, lr = a
         checkpoint = checkpoint+'/dae_kl'
 
     for epoch in range(epochs):
+        model_dae.train()
         print('\nEpoch: [%d |%d] ' % (epoch + 1, epochs))
 
-        for batch_idx, (inputs, targets) in enumerate(data_loader):
-            noise_inputs = noise(inputs)
-            noise_inputs,inputs, targets =noise_inputs.cuda(), inputs.cuda(), targets.cuda()
-            noise_inputs,inputs, targets = Variable(noise_inputs), Variable(inputs), Variable(targets)
+        losses, top1, top3 = train_dae_step(trainloader, model_clf,model_comb, criterion, optimizer,dae_loss,tempr,std)
 
-            outputs = model_clf(inputs)
-            noise_ = model_dae(noise_inputs)
-            denoise_inputs = torch.clamp(noise_inputs + noise_,0,1)
-            outputs_ = model_clf(denoise_inputs)
+        # Test models
+        # test error for clean images and reconstructed images
+        test_loss, test_acc, test_acc3 = test_dae(model_clf,model_comb, testloader, criterion,dae_loss, False,tempr)
+        #test error for Ganssian corrupted images
+        test_loss_n, test_acc_n, test_acc3_n = test_clf(model_clf,model_comb, testloader,criterion,dae_loss, std, tempr)
 
-            if dae_loss == 'KL':
-                loss = criterion(outputs_/tempr,outputs/tempr)
-            elif dae_loss =='KL_reverse':
-                loss = criterion(outputs_/tmpr,outputs/tempr,True)
-            else:
-                loss = criterion(outputs,outputs_)
+        print('Training/cleanTest/noiseTest [loss] %.4f / %.4f / %.4f, [top1] %.2f / %.2f / %.2f, [top3] %.2f / %.2f / %.2f' %
+              (losses.avg, test_loss,test_loss_n, 100 * top1.avg, 100 * test_acc, 100*test_acc_n, \
+               100 * top3.avg, 100 * test_acc3, 100*test_acc3_n))
+        # print('Test [loss] %.4f, [top1]')
+        logger.append([losses.avg, top1.avg, top3.avg, test_loss_n, test_acc_n, test_acc3_n])
+        # save_model
+        is_best = top1.avg > best_acc
+        best_acc = max(test_acc, best_acc)
+        save_checkpoint({
+            'epoch': epoch + 1,
+            'state_dict': model_comb.state_dict(),
+            'acc': top1.avg,
+            'best_acc': best_acc,
+            'optimizer': optimizer.state_dict(),
 
-
-            # Training the models
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
+        }, is_best, checkpoint=checkpoint)
+    logger.close()
+    logger.plot()
+    savefig()
+    print('Best acc:')
+    print(best_acc)
             # Test accuracy!
 
+def train_dae_step(data_loader, model_clf,model_comb,criterion, optimizer,dae_loss,tempr,std):
 
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top3 = AverageMeter()
+
+    for batch_idx, (inputs, targets) in enumerate(data_loader):
+        noise_inputs = noise(inputs,std)
+        noise_inputs, inputs, targets = noise_inputs.cuda(), inputs.cuda(), targets.cuda()
+        noise_inputs, inputs, targets = Variable(noise_inputs), Variable(inputs), Variable(targets)
+
+        outputs = model_clf(inputs)
+        #noise_ = model_dae(noise_inputs)
+        #denoise_inputs = torch.clamp(noise_inputs + noise_, 0, 1)
+        #denoise_inputs = model_dae(noise_inputs)
+        outputs_ = model_comb(noise_inputs)
+
+        if dae_loss == 'KL':
+            loss = criterion(outputs_ / tempr, outputs / tempr)
+        elif dae_loss == 'KL_reverse':
+            loss = criterion(outputs_ / tempr, outputs / tempr, True)
+        else:
+            loss = criterion(outputs, outputs_)
+
+        prec1, prec3 = accuracy(outputs_.data, targets.data, topk=(1, 3))
+        if (batch_idx) % 10 == 1:
+            print("Training [loss] %.4f [Acc] top1: %.2f , top3: %.2f " % (loss, 100 * prec1, 100 * prec3))
+
+        losses.update(loss.data[0], inputs.size(0))
+        top1.update(prec1[0], inputs.size(0))
+        top3.update(prec3[0], inputs.size(0))
+
+        # Training the models
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    return losses, top1, top3
 
 
 """
@@ -364,7 +396,6 @@ def test_clf(model, testloader, criterion):
     model.eval()
 
     for batch_idx, (inputs, targets) in enumerate(testloader):
-
         inputs, targets = inputs.cuda(), targets.cuda()
         inputs, targets = Variable(inputs, volatile = True), Variable(targets)
 
@@ -376,6 +407,40 @@ def test_clf(model, testloader, criterion):
         top3.update(prec3[0],inputs.size(0))
 
 
+
+    return losses.avg, top1.avg, top3.avg
+
+def test_dae(model_clf, model_comb, testloader, criterion,dae_loss, noise=False, tempr=1):
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top3 = AverageMeter()
+
+    model_clf.eval()
+    model_comb.eval()
+
+    for batch_idx, (inputs, targets) in enumerate(testloader):
+        if noise>0:
+            noise_inputs = noise(inputs, noise)
+        else:
+            noise_inputs = inputs
+        noise_inputs, inputs, targets = noise_inputs.cuda(), inputs.cuda(), targets.cuda()
+        noise_inputs, inputs, targets = Variable(noise_inputs), Variable(inputs), Variable(targets)
+
+        outputs = model_clf(inputs)
+        outputs_ = model_comb(noise_inputs)
+
+        if dae_loss == 'KL':
+            loss = criterion(outputs_ / tempr, outputs / tempr)
+        elif dae_loss == 'KL_reverse':
+            loss = criterion(outputs_ / tempr, outputs / tempr, True)
+        else:
+            loss = criterion(outputs, outputs_)
+
+        prec1, prec3 = accuracy(outputs_.data, targets.data, topk=(1, 3))
+        losses.update(loss.data[0],inputs.size(0))
+        top1.update(prec1[0],inputs.size(0))
+        top3.update(prec3[0],inputs.size(0))
+
     return losses.avg, top1.avg, top3.avg
 
 
@@ -386,9 +451,9 @@ def save_checkpoint(state, is_best, checkpoint='checkpoint',filename='checkpoint
         shutil.copyfile(filepath, os.path.join(checkpoint,'model_best.pth.tar'))
 
 
-def noise(X, noise_std):
+def noise(X, noise_std,low=0, high=1):
     X_noise = X+noise_std*torch.randn_like(X)
-    return torch.clamp(X_noise, 0,1)
+    return torch.clamp(X_noise, low,high)
 
 
 if __name__ =='__main__':
