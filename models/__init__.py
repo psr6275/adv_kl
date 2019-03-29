3217c0e6e6a95ac57f5e52796a09a6f5e68489fb
saerom <psr6275@naver.com>
2019-03-29 18:03:31 +0900

    revising attack part

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from __future__ import absolute_import
 from .mnist import *
 from .cifar import *
 from .combine import *
+from .attacks import *
--- models/attacks.py
+++ models/attacks.py
@@ -6,7 +6,11 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 from torchvision import datasets, transforms
-from ..utils.eval import TripleDataSet
+import sys
+sys.path.append("../")
+from utils import TripleDataSet
+
+__all__ = ['WhiteBox']
 
 def pgd_attack(target_model,X,y,params):
     niter = params['niter']
@@ -34,32 +38,37 @@ def pgd_attack(target_model,X,y,params):
 
 
 
-class whitebox:
+class WhiteBox:
     def __init__(self, target_model,dataloader,attacker = 'pgd',params = {'eps':8/255,'niter':20, 'alpha':2/255,'normalize':'0/1'}):
         self.target_model = target_model.eval()
         self.dataloader = dataloader
         self.params = params
+        print("<<<",attacker ," Attack>>>")
         if attacker is "pgd":
             self.attacker = pgd_attack
-        self.attack_model()
+        self.tripleDataSet = self.attack_model()
 
     def attack_model(self):
         adv_list =[]
         input_list = []
         target_list = []
+        num_batch = len(self.dataloader)
         for i, (inputs, targets) in enumerate(self.dataloader):
             inputs = Variable(inputs).cuda()
             targets = Variable(targets).cuda()
             adv_inputs = self.attacker(self.target_model,inputs,targets,self.params)
 
-            input_list.append(inputs.data)
-            target_list.append(targets.data)
-            adv_list.append(adv_inputs.data)
-
+            input_list.append(inputs)
+            target_list.append(targets)
+            adv_list.append(adv_inputs)
+            print("attacking batch [%.d/%.d]"%(i,num_batch),end='\r')
+        print("\n")
         adv_tensor = torch.cat(adv_list,0)
         input_tensor = torch.cat(input_list,0)
         target_tensor = torch.cat(target_list,0)
-
+        print(target_list)
+        print(target_tensor)
+        print("complete attacks!")
         return TripleDataSet(input_tensor,adv_tensor,target_tensor)
 
 
